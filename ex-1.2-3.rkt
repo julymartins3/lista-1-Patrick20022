2fc188867afd0051f7d8ecc8af301f38ad026234
Patrick <patrick.saul00@gmail.com>
2020-03-06 12:45:00 -0300

    exercícios

--- ex-1.2-3.rkt
+++ ex-1.2-3.rkt
@@ -2,8 +2,9 @@
 
 ;; exercicio 1.2
 ;; Ver em https://web.mit.edu/alexmv/6.037/sicp.pdf 
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))
 
-...
 
 ;; exercicio 1.3
 
@@ -13,8 +14,13 @@
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
 
-(define (sum-of-squares-max3 a b c)
-  ...)
+(define (sum-of-squares-max3 x y z)
+  (cond ((and (<= x y) (< x z)) (+ (* y y) (* z z)))
+        ((and (<= y x) (< y z)) (+ (* x x) (* z z)))
+        ((and (<= z x) (< z y)) (+ (* x x) (* y y)))))
+  
 
 (provide sum-of-squares-max3)
 
+  
+  
--- ex-1.8.rkt
+++ ex-1.8.rkt
@@ -1,27 +1,16 @@
 #lang racket
-
-(define (abs x)
-  (if (< x 0) (- x) x))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (square b)
-  (* b b))
-
 (define (cube-root x)
-  (define (good-enough? guess)
-    (< (abs (- (square guess) x)) 0.00001))
+    (define min 0.00001)
+    (define (cube-iter guess)
+        (if (good-enough? guess)
+            guess
+            (cube-iter (guess-new guess))))
+    (define (good-enough? guess)
+        (< (abs (- (* guess guess guess) x )) min))
+    (define (guess-new y)
+        (/ (+ (/ x (* y y)) y y) 3))
+    (cube-iter 1.0))
 
-  (define (good-enough2? ...)
-    ...)
 
-  (define (improve guess)
-    ...)
-
-  (define (aux guess)
-    ...)
-
-  (aux 1.0))
 
 (provide cube-root)
